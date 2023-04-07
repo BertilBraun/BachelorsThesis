e1baafe148170549e229e148235b53a9503873a8
Bertil <bertil.braun.private@gmail.com>
2023-04-07 18:43:51 +0200

    Revised Runner

--- runner.py
+++ runner.py
@@ -6,10 +6,8 @@ from concurrent.futures import ProcessPoolExecutor
 
 MS_OF_24_HOURS = 24 * 60 * 60 * 1000
 
-JJBMC_CMD = "java -jar ../../../../JJBMC.jar -mas {mas} -u {u}{fil} -sc -tr -c -kt -timeout={timeout} BlockQuickSort.java {function}"
+JJBMC_CMD = "java -jar ../../../../JJBMC.jar -mas {mas} -u {u}{inline} -sc -tr -c -kt -timeout={timeout} BlockQuickSort.java {function}"
 
-JBMC_CMD = "jbmc {file} --unwind {u} --java-max-input-array-length {mas} --function {function} --trace --stop-on-fail --timestamp monotonic"
-    
 FUNCTIONS = [
     "swap",
     "sortPair",
@@ -23,14 +21,14 @@ FUNCTIONS = [
     "permutation",
 ]
 
-BOUNDS = list(range(3, 4)) # TODO 10
+BOUNDS = list(range(3, 10))
 
 HOME_FOLDER = os.getcwd()
 BASE_FOLDER = HOME_FOLDER + "/bqs/results"
 
 MAX_WORKERS = 4
 
-def process_JJBMC_example(folder, bound, function, force_loop_inline):
+def process_JJBMC_example(folder, bound, function, inline_arg):
 
     # Copy the java file in the folder
     
@@ -43,7 +41,7 @@ def process_JJBMC_example(folder, bound, function, force_loop_inline):
         u=bound + 2, 
         timeout=MS_OF_24_HOURS, 
         function=function, 
-        fil=' -fil' if force_loop_inline else ''
+        fil=inline_arg
     )
     print("Running command: " + cmd)
     
@@ -51,7 +49,7 @@ def process_JJBMC_example(folder, bound, function, force_loop_inline):
     os.chdir(folder)
     p = subprocess.Popen(cmd.split(' '), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    with open("JJBMC output{0}.txt".format(' fil' if force_loop_inline else ''), "w") as f:
+    with open("JJBMC output{0}.txt".format(inline_arg), "w") as f:
         # Write stdout and stderr to file
         stdout = p.stdout.read().decode("utf-8")
         stderr = p.stderr.read().decode("utf-8")
@@ -60,57 +58,17 @@ def process_JJBMC_example(folder, bound, function, force_loop_inline):
         f.write(stdout)
         f.write(stderr)
         
-    try:
-        p.wait(timeout=MS_OF_24_HOURS / 1000)
-    except subprocess.TimeoutExpired:
-        print(f"JJBMC process (force_loop_inline={force_loop_inline}) timed out after 24 hours.")
-        p.terminate()       
+    p.wait(timeout=MS_OF_24_HOURS / 1000)
     os.chdir(HOME_FOLDER)
     
-    
-def process_JBMC_example(folder, file, bound, function):
-        # Compile the Java file
-    file = f"JBMC_BlockQuickSort_{function}"
-    os.system(f"cp {HOME_FOLDER}/bqs/jbmc/{file}.java {folder}/{file}.java")
-
-    # Compile the Java file
-    os.system(f"javac {folder}/{file}.java")
-    
-    cmd = JBMC_CMD.format(
-        file=file,
-        mas=bound,
-        u=bound + 2,
-        function=function
-    )
-    print("Running command: " + cmd)
-
-    os.chdir(folder)
-    p = subprocess.Popen(cmd.split(' '), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-    with open("JBMC output.txt", "w") as f:
-        # Write stdout and stderr to file
-        stdout = p.stdout.read().decode("utf-8")
-        stderr = p.stderr.read().decode("utf-8")
-        print(stdout)
-        print(stderr)
-        f.write(stdout)
-        f.write(stderr)
-
-    try:
-        p.wait(timeout=MS_OF_24_HOURS / 1000)
-    except subprocess.TimeoutExpired:
-        print("JBMC process timed out after 24 hours.")
-        p.terminate()
-    os.chdir(HOME_FOLDER)
-
 
 def worker(bound, function):
     folder = f"{BASE_FOLDER}/{bound}/{function}"
     os.makedirs(folder, exist_ok=True)
 
-    process_JJBMC_example(folder, bound, function, True)
-    process_JJBMC_example(folder, bound, function, False)
-    process_JBMC_example(folder, bound, function)
+    process_JJBMC_example(folder, bound, function, '')
+    process_JJBMC_example(folder, bound, function, ' -fil')
+    process_JJBMC_example(folder, bound, function, ' -fi')
 
 if __name__ == "__main__":
     tasks = []
