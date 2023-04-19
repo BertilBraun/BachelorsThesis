54f8a41d7ed4d2f2303c74d2aba5d9d28d101d7f
Bertil <bertil.braun.private@gmail.com>
2023-04-19 21:02:31 +0200

    Better Runner, add ability to specify bounds per func

--- runner.py
+++ runner.py
@@ -10,6 +10,12 @@ from concurrent.futures import ProcessPoolExecutor
 
 # this file should run a command for multiple inputs sequentially
 
+
+HOME_FOLDER = os.getcwd()
+BASE_FOLDER = HOME_FOLDER + "/bqs/results"
+MAX_BOUND = 100
+ITERATIONS = 5
+
 MS_OF_10_HOURS = 10 * 60 * 60 * 1000
 
 JJBMC_CMD = "java -jar ../../../../../JJBMC.jar -mas {mas} -u {u}{inline} -tr -c -kt -timeout={timeout} BlockQuickSort.java {function} -j=--stop-on-fail"
@@ -20,28 +26,21 @@ HARD_WORKERS = 12
 
 TASKS = [
     (EASY_WORKERS, [
-        "swap",
-        "sortPair",
-    ], list(range(1, 15)), 3),
+        ("swap", list(range(1, 15)), 3),
+        ("sortPair", list(range(1, 15)), 3),
+        ("medianOf3", list(range(1, 14)), 3),
+    ]),
     (MEDIUM_WORKERS, [
-        "partition",
-        "medianOf3",
-        "insertionSort",
-    ], list(range(1, 12)), 3),
+        ("partition", list(range(1, 13)), 3),  # rewrite to add bounds per function, not per block
+        ("insertionSort", list(range(1, 9)), 3),
+    ]),
     (HARD_WORKERS, [
-        "permutation",
-        "hoareBlockPartition",
-    ], list(range(1, 8)), 2),
-    (HARD_WORKERS, [
-        "quickSortRec",
-        "quickSortRecImpl",
-    ], list(range(1, 7)), 2),
+        ("permutation", list(range(1, 7)), 2),
+        ("hoareBlockPartition", list(range(1, 7)), 2),  # TODO no idea how slow this is
+        ("quickSortRec", list(range(1, 8)), 2),
+        ("quickSortRecImpl", list(range(1, 6)), 2),
+    ])
 ]
-
-HOME_FOLDER = os.getcwd()
-BASE_FOLDER = HOME_FOLDER + "/bqs/results"
-MAX_BOUND = 100
-ITERATIONS = 5
 
 failed_examples = {}
 
@@ -99,7 +98,7 @@ def generate_tasks(iteration, bound, function):
         tasks.append((folder, bound, function, ''))
     if failed_examples.get((function, ' -fil'), MAX_BOUND) > bound:
         tasks.append((folder, bound, function, ' -fil'))
-    if failed_examples.get((function, ' -fi'), MAX_BOUND) > bound and (function not in ['quickSortRec', 'quickSortRecImpl'] or bound < 4):
+    if failed_examples.get((function, ' -fi'), MAX_BOUND) > bound:
         tasks.append((folder, bound, function, ' -fi'))
 
     return tasks
@@ -115,11 +114,12 @@ def run(workers, tasks):
 
 if __name__ == "__main__":
     for i in range(ITERATIONS):
-        for (workers, functions, bounds, times_per_iteration) in TASKS:
+        for (workers, functions) in TASKS:
             tasks = []
-            for j in range(times_per_iteration):
-                for bound in bounds:
-                    for function in functions:
-                        tasks.extend(generate_tasks(i * times_per_iteration + j, bound, function))
+            for (function, bounds, times_per_iteration) in functions:
+                for j in range(times_per_iteration):
+                    for bound in bounds:
+                        tasks += generate_tasks(i * times_per_iteration + j, bound, function)
 
+            tasks = list(sorted(tasks, key=lambda x: x[1]))
             run(workers, tasks)
