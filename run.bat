60b26fd741dda9a76bc717e26274339fa8a8aff6
Bertil <bertil.braun.private@gmail.com>
2023-04-19 12:22:03 +0200

    Small runner script for testing

--- run.bat
+++ run.bat
@@ -1,2 +1,24 @@
-docker cp "%1.java" "pedantic_darwin:/JJBMC/%1.java"
-docker exec pedantic_darwin java -jar JJBMC.jar -tr "%1.java" %2
+@echo off
+setlocal enabledelayedexpansion
+
+for /F "tokens=1-4 delims=:. " %%a in ("%TIME%") do (
+  set hour=%%a
+  set minute=%%b
+  set second=%%c
+)
+
+if %hour% LSS 10 set hour=0%hour%
+
+for /F "tokens=1 delims=," %%a in ("%second%") do (
+  set second=%%a
+)
+
+set current_time=%hour%_%minute%_%second%
+REM create a folder bqs/run/"current_time" to store the results
+mkdir bqs\run\%current_time%
+REM copy the source code to the folder
+copy bqs\BlockQuickSort.java bqs\run\%current_time%
+
+wsl ./gradlew fatJar
+
+java -jar JJBMC.jar -mas 6 -u 8 -tr -c -kt -timeout=18000000 bqs\run\%current_time%\BlockQuickSort.java hoareBlockPartition -rv upper -rv lowerI -rv indexL -rv indexR -rv array -rv startLeft -rv startRight -rv numLeft -rv numRight -rv num -rv originalBegin -rv originalEnd -rv swapI -rv swapJ -rv begin -rv end -rv originalArray
